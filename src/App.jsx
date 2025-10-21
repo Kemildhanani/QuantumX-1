64ddf9fb6405cc7007ec34985cf1df2fa448839e
Kemil Dhanani <kemildhanani111@gmail.com>
2025-10-21 09:15:28 +0530

    UI changes fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-
 import Layouts from "./layouts/Layouts";
 
-function App() {
-  return (
-    <div className="">
-      <Router>
-        <Layouts>
-          <Routes>
-            {/* Home */}
-            <Route path="/" element={<Home />} />
-            {/* Other Pages */}
-            {/* <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} /> */}
 
-            {/* 404 Page */}
-            {/* <Route
-            path="*"
-            element={
-              <h1 className="text-center mt-20">404 - Page Not Found</h1>
-              }
-              /> */}
-          </Routes>
-        </Layouts>
-      </Router>
-    </div>
+function App() {
+
+  return (
+    <>
+ 
+        <Router>
+          <Layouts>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Layouts>
+        </Router>
+    </>
   );
 }
 
